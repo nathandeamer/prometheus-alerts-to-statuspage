1fd7dc791d185edba24942dde705e1ff06ff5d08
nathandeamer <nd@nathandeamer.com>
2023-04-04 14:30:25 +0100

    Reverted some logs.  Log alert as json

--- src/main/java/com/nathandeamer/prometheustostatuspage/alertmanager/WebhookController.java
+++ src/main/java/com/nathandeamer/prometheustostatuspage/alertmanager/WebhookController.java
@@ -1,5 +1,6 @@
 package com.nathandeamer.prometheustostatuspage.alertmanager;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.nathandeamer.prometheustostatuspage.PrometheusToStatusPageService;
 import com.nathandeamer.prometheustostatuspage.alertmanager.dto.AlertWrapper;
 import lombok.RequiredArgsConstructor;
@@ -15,9 +16,11 @@ public class WebhookController {
 
     private final PrometheusToStatusPageService prometheusToStatusPageService;
 
+    private final ObjectMapper objectMapper;
+
     @PostMapping("/alert")
-    public void alert(@RequestBody AlertWrapper alert) {
-        log.info("{}", alert);
+    public void alert(@RequestBody AlertWrapper alert) throws Exception {
+        log.info("{}", objectMapper.writeValueAsString(alert));
         prometheusToStatusPageService.alert(alert);
     }
 
