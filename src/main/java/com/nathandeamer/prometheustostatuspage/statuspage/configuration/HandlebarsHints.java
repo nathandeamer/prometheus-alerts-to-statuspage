0a6fa67ee6d6accb002fac9abde54fe724f05005
nathandeamer <nd@nathandeamer.com>
2023-04-13 16:48:30 +0100

    Added native hint so that .name function on enum is included in the AOT - so it can be used by handlebars

--- src/main/java/com/nathandeamer/prometheustostatuspage/statuspage/configuration/HandlebarsHints.java
+++ src/main/java/com/nathandeamer/prometheustostatuspage/statuspage/configuration/HandlebarsHints.java
@@ -1,12 +1,17 @@
 package com.nathandeamer.prometheustostatuspage.statuspage.configuration;
 
+import com.nathandeamer.prometheustostatuspage.alertmanager.dto.Status;
+import lombok.SneakyThrows;
+import org.springframework.aot.hint.ExecutableMode;
 import org.springframework.aot.hint.RuntimeHints;
 import org.springframework.aot.hint.RuntimeHintsRegistrar;
 
 public class HandlebarsHints implements RuntimeHintsRegistrar {
 
+    @SneakyThrows
     @Override
     public void registerHints(RuntimeHints hints, ClassLoader classLoader) {
         hints.resources().registerPattern("helpers.nashorn.js");
+        hints.reflection().registerMethod(Status.class.getMethod("name"), ExecutableMode.INVOKE);
     }
 }
